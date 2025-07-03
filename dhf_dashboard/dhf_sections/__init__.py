fff277c3fd35db82f9b97059358333a111870ca6
LEAN-arch <jbautistamsc@gmail.com>
2025-07-03 02:51:25 -0700

    Update __init__.py

--- dhf_dashboard/dhf_sections/__init__.py
+++ dhf_dashboard/dhf_sections/__init__.py
@@ -1,1 +1,11 @@
-
+# Make all render functions available under the dhf_sections namespace
+from . import design_plan
+from . import design_risk_management
+from . import human_factors
+from . import design_inputs
+from . import design_outputs
+from . import design_reviews
+from . import design_verification
+from . import design_validation
+from . import design_transfer
+from . import design_changes
